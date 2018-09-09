8d862af73bf0f242641811b98da128589e18505f
RedLeaves <redleavessun@gmail.com>
2018-09-09 12:50:47 +0800

    Allow to change serial port parameter when open
    Allow to change baudrate, databits, parity and stopbits when
    serial port open.

--- serialport/src/main/jni/serial-port-lib.c
+++ serialport/src/main/jni/serial-port-lib.c
@@ -27,36 +27,34 @@ static const char *TAG = "serial-port";
 #define STOPBITS_1   1
 #define STOPBITS_2   2
 
-static struct termios termios_p;
-
 JNIEXPORT jobject JNICALL
 Java_com_redleaves_serialport_SerialPort_serial_1port_1open(JNIEnv *env, jobject instance,
                                                             jstring path_) {
 
-    int fd;
+    int descriptor;
 
     const char *path = (*env)->GetStringUTFChars(env, path_, 0);
     LOGD("file path: %s", path);
-    fd = open(path, O_RDWR);
-    LOGD("fd: %d", fd);
-    if (-1 == fd) {
-        LOGE("open failed");
+    descriptor = open(path, O_RDWR);
+    if (-1 == descriptor) {
+        LOGE("fd: %d, open failed", descriptor);
         return NULL;
     }
     (*env)->ReleaseStringUTFChars(env, path_, path);
 
+    struct termios termios_p;
     memset(&termios_p, 0, sizeof(struct termios));
-    if (-1 == tcgetattr(fd, &termios_p)) {
-        LOGE("tcgetattr failed");
-        close(fd);
+    if (-1 == tcgetattr(descriptor, &termios_p)) {
+        LOGE("fd: %d, tcgetattr failed", descriptor);
+        close(descriptor);
         return NULL;
     }
 
     cfmakeraw(&termios_p);
 
-    if (-1 == tcsetattr(fd, TCSANOW, &termios_p)) {
-        LOGE("tcsetattr failed");
-        close(fd);
+    if (-1 == tcsetattr(descriptor, TCSANOW, &termios_p)) {
+        LOGE("fd: %d, tcsetattr failed", descriptor);
+        close(descriptor);
         return NULL;
     }
 
@@ -64,7 +62,7 @@ Java_com_redleaves_serialport_SerialPort_serial_1port_1open(JNIEnv *env, jobject
     jmethodID FileDescriptorMethodID = (*env)->GetMethodID(env, FileDescriptorClass, "<init>", "()V");
     jfieldID descriptorFieldID = (*env)->GetFieldID(env, FileDescriptorClass, "descriptor", "I");
     jobject fileDescriptor = (*env)->NewObject(env, FileDescriptorClass, FileDescriptorMethodID);
-    (*env)->SetIntField(env, fileDescriptor, descriptorFieldID, (jint) fd);
+    (*env)->SetIntField(env, fileDescriptor, descriptorFieldID, (jint) descriptor);
 
     return fileDescriptor;
 }
@@ -76,10 +74,9 @@ Java_com_redleaves_serialport_SerialPort_serial_1port_1close(JNIEnv *env, jobjec
     jclass FileDescriptorClass = (*env)->FindClass(env, "java/io/FileDescriptor");
     jfieldID descriptorFieldID = (*env)->GetFieldID(env, FileDescriptorClass, "descriptor", "I");
     jint descriptor = (*env)->GetIntField(env, fd, descriptorFieldID);
-    LOGD("fd: %d", descriptor);
 
     if (-1 == close(descriptor)) {
-        LOGE("close failed");
+        LOGE("fd: %d, close failed", descriptor);
         return JNI_FALSE;
     }
 
@@ -93,54 +90,58 @@ Java_com_redleaves_serialport_SerialPort_serial_1port_1set_1baudrate(JNIEnv *env
     jclass FileDescriptorClass = (*env)->FindClass(env, "java/io/FileDescriptor");
     jfieldID descriptorFieldID = (*env)->GetFieldID(env, FileDescriptorClass, "descriptor", "I");
     jint descriptor = (*env)->GetIntField(env, fd, descriptorFieldID);
-    LOGD("fd: %d", descriptor);
 
-//    struct termios termios_p;
-//    memset(&termios_p, 0, sizeof(struct termios));
-//    if (-1 == tcgetattr(descriptor, &termios_p)) {
-//        LOGE("tcgetattr failed");
-//        return;
-//    }
+    struct termios termios_p;
+    memset(&termios_p, 0, sizeof(struct termios));
+    if (-1 == tcgetattr(descriptor, &termios_p)) {
+        LOGE("fd: %d, tcgetattr failed", descriptor);
+        return;
+    }
 
     speed_t speed;
 
     switch (baudRate) {
-        case 0:       { speed = B0;       LOGD("baudRate: %d", baudRate); break; }
-        case 50:      { speed = B50;      LOGD("baudRate: %d", baudRate); break; }
-        case 75:      { speed = B75;      LOGD("baudRate: %d", baudRate); break; }
-        case 110:     { speed = B110;     LOGD("baudRate: %d", baudRate); break; }
-        case 134:     { speed = B134;     LOGD("baudRate: %d", baudRate); break; }
-        case 150:     { speed = B150;     LOGD("baudRate: %d", baudRate); break; }
-        case 200:     { speed = B200;     LOGD("baudRate: %d", baudRate); break; }
-        case 300:     { speed = B300;     LOGD("baudRate: %d", baudRate); break; }
-        case 600:     { speed = B600;     LOGD("baudRate: %d", baudRate); break; }
-        case 1200:    { speed = B1200;    LOGD("baudRate: %d", baudRate); break; }
-        case 1800:    { speed = B1800;    LOGD("baudRate: %d", baudRate); break; }
-        case 2400:    { speed = B2400;    LOGD("baudRate: %d", baudRate); break; }
-        case 4800:    { speed = B4800;    LOGD("baudRate: %d", baudRate); break; }
-        case 9600:    { speed = B9600;    LOGD("baudRate: %d", baudRate); break; }
-        case 19200:   { speed = B19200;   LOGD("baudRate: %d", baudRate); break; }
-        case 38400:   { speed = B38400;   LOGD("baudRate: %d", baudRate); break; }
-        case 57600:   { speed = B57600;   LOGD("baudRate: %d", baudRate); break; }
-        case 115200:  { speed = B115200;  LOGD("baudRate: %d", baudRate); break; }
-        case 230400:  { speed = B230400;  LOGD("baudRate: %d", baudRate); break; }
-        case 460800:  { speed = B460800;  LOGD("baudRate: %d", baudRate); break; }
-        case 500000:  { speed = B500000;  LOGD("baudRate: %d", baudRate); break; }
-        case 576000:  { speed = B576000;  LOGD("baudRate: %d", baudRate); break; }
-        case 921600:  { speed = B921600;  LOGD("baudRate: %d", baudRate); break; }
-        case 1000000: { speed = B1000000; LOGD("baudRate: %d", baudRate); break; }
-        case 1152000: { speed = B1152000; LOGD("baudRate: %d", baudRate); break; }
-        case 1500000: { speed = B1500000; LOGD("baudRate: %d", baudRate); break; }
-        case 2000000: { speed = B2000000; LOGD("baudRate: %d", baudRate); break; }
-        case 2500000: { speed = B2500000; LOGD("baudRate: %d", baudRate); break; }
-        case 3000000: { speed = B3000000; LOGD("baudRate: %d", baudRate); break; }
-        case 3500000: { speed = B3500000; LOGD("baudRate: %d", baudRate); break; }
-        case 4000000: { speed = B4000000; LOGD("baudRate: %d", baudRate); break; }
+        case 0:       { speed = B0;       LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 50:      { speed = B50;      LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 75:      { speed = B75;      LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 110:     { speed = B110;     LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 134:     { speed = B134;     LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 150:     { speed = B150;     LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 200:     { speed = B200;     LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 300:     { speed = B300;     LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 600:     { speed = B600;     LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 1200:    { speed = B1200;    LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 1800:    { speed = B1800;    LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 2400:    { speed = B2400;    LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 4800:    { speed = B4800;    LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 9600:    { speed = B9600;    LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 19200:   { speed = B19200;   LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 38400:   { speed = B38400;   LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 57600:   { speed = B57600;   LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 115200:  { speed = B115200;  LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 230400:  { speed = B230400;  LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 460800:  { speed = B460800;  LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 500000:  { speed = B500000;  LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 576000:  { speed = B576000;  LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 921600:  { speed = B921600;  LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 1000000: { speed = B1000000; LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 1152000: { speed = B1152000; LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 1500000: { speed = B1500000; LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 2000000: { speed = B2000000; LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 2500000: { speed = B2500000; LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 3000000: { speed = B3000000; LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 3500000: { speed = B3500000; LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
+        case 4000000: { speed = B4000000; LOGD("fd: %d, baudRate: %d", descriptor, baudRate); break; }
         default:      { LOGE("baudRate: invalid parameter"); return; }
     }
 
     if (-1 == cfsetspeed(&termios_p, speed)) {
-        LOGE("cfsetspeed failed");
+        LOGE("fd: %d, cfsetspeed failed", descriptor);
+        return;
+    }
+
+    if (-1 == tcsetattr(descriptor, TCSANOW, &termios_p)) {
+        LOGE("fd: %d, tcsetattr failed", descriptor);
         return;
     }
 }
@@ -152,46 +153,45 @@ Java_com_redleaves_serialport_SerialPort_serial_1port_1set_1databits(JNIEnv *env
     jclass FileDescriptorClass = (*env)->FindClass(env, "java/io/FileDescriptor");
     jfieldID descriptorFieldID = (*env)->GetFieldID(env, FileDescriptorClass, "descriptor", "I");
     jint descriptor = (*env)->GetIntField(env, fd, descriptorFieldID);
-    LOGD("fd: %d", descriptor);
 
-//    struct termios termios_p;
-//    memset(&termios_p, 0, sizeof(struct termios));
-//    if (-1 == tcgetattr(descriptor, &termios_p)) {
-//        LOGE("tcgetattr failed");
-//        return;
-//    }
+    struct termios termios_p;
+    memset(&termios_p, 0, sizeof(struct termios));
+    if (-1 == tcgetattr(descriptor, &termios_p)) {
+        LOGE("fd: %d, tcgetattr failed", descriptor);
+        return;
+    }
 
     termios_p.c_cflag &= ~CSIZE;
 
     switch (dataBits) {
         case DATABITS_5: {
             termios_p.c_cflag |= CS5;
-            LOGD("dataBits: %d", dataBits);
+            LOGD("fd: %d, dataBits: %d", descriptor, dataBits);
             break;
         }
         case DATABITS_6: {
             termios_p.c_cflag |= CS6;
-            LOGD("dataBits: %d", dataBits);
+            LOGD("fd: %d, dataBits: %d", descriptor, dataBits);
             break;
         }
         case DATABITS_7: {
             termios_p.c_cflag |= CS7;
-            LOGD("dataBits: %d", dataBits);
+            LOGD("fd: %d, dataBits: %d", descriptor, dataBits);
             break;
         }
         case DATABITS_8: {
             termios_p.c_cflag |= CS8;
-            LOGD("dataBits: %d", dataBits);
+            LOGD("fd: %d, dataBits: %d", descriptor, dataBits);
             break;
         }
         default: {
-            LOGE("dataBits: invalid parameter");
+            LOGE("fd: %d, dataBits: invalid parameter", descriptor);
             return;
         }
     }
 
     if (-1 == tcsetattr(descriptor, TCSANOW, &termios_p)) {
-        LOGE("tcsetattr failed");
+        LOGE("fd: %d, tcsetattr failed", descriptor);
         return;
     }
 }
@@ -203,57 +203,56 @@ Java_com_redleaves_serialport_SerialPort_serial_1port_1set_1parity(JNIEnv *env, 
     jclass FileDescriptorClass = (*env)->FindClass(env, "java/io/FileDescriptor");
     jfieldID descriptorFieldID = (*env)->GetFieldID(env, FileDescriptorClass, "descriptor", "I");
     jint descriptor = (*env)->GetIntField(env, fd, descriptorFieldID);
-    LOGD("fd: %d", descriptor);
 
-//    struct termios termios_p;
-//    memset(&termios_p, 0, sizeof(struct termios));
-//    if (-1 == tcgetattr(descriptor, &termios_p)) {
-//        LOGE("tcgetattr failed");
-//        return;
-//    }
+    struct termios termios_p;
+    memset(&termios_p, 0, sizeof(struct termios));
+    if (-1 == tcgetattr(descriptor, &termios_p)) {
+        LOGE("fd: %d, tcgetattr failed", descriptor);
+        return;
+    }
 
     switch (parity) {
         case PARITY_NONE: {
             termios_p.c_cflag &= ~PARENB;
-            LOGD("parity: None");
+            LOGD("fd: %d, parity: None", descriptor);
             break;
         }
         case PARITY_ODD: {
             termios_p.c_cflag |= PARENB;
             termios_p.c_cflag &= ~CMSPAR;
             termios_p.c_cflag |= PARODD;
-            LOGD("parity: Odd");
+            LOGD("fd: %d, parity: Odd", descriptor);
             break;
         }
         case PARITY_EVEN: {
             termios_p.c_cflag |= PARENB;
             termios_p.c_cflag &= ~CMSPAR;
             termios_p.c_cflag &= ~PARODD;
-            LOGD("parity: Even");
+            LOGD("fd: %d, parity: Even", descriptor);
             break;
         }
         case PARITY_MARK: {
             termios_p.c_cflag |= PARENB;
             termios_p.c_cflag |= CMSPAR;
             termios_p.c_cflag |= PARODD;
-            LOGD("parity: Mark");
+            LOGD("fd: %d, parity: Mark", descriptor);
             break;
         }
         case PARITY_SPACE: {
             termios_p.c_cflag |= PARENB;
             termios_p.c_cflag |= CMSPAR;
             termios_p.c_cflag &= ~PARODD;
-            LOGD("parity: Space");
+            LOGD("fd: %d, parity: Space", descriptor);
             break;
         }
         default: {
-            LOGE("parity: invalid parameter");
+            LOGE("fd: %d, parity: invalid parameter", descriptor);
             return;
         }
     }
 
     if (-1 == tcsetattr(descriptor, TCSANOW, &termios_p)) {
-        LOGE("tcsetattr failed");
+        LOGE("fd: %d, tcsetattr failed", descriptor);
         return;
     }
 }
@@ -265,34 +264,33 @@ Java_com_redleaves_serialport_SerialPort_serial_1port_1set_1stopbits(JNIEnv *env
     jclass FileDescriptorClass = (*env)->FindClass(env, "java/io/FileDescriptor");
     jfieldID descriptorFieldID = (*env)->GetFieldID(env, FileDescriptorClass, "descriptor", "I");
     jint descriptor = (*env)->GetIntField(env, fd, descriptorFieldID);
-    LOGD("fd: %d", descriptor);
 
-//    struct termios termios_p;
-//    memset(&termios_p, 0, sizeof(struct termios));
-//    if (-1 == tcgetattr(descriptor, &termios_p)) {
-//        LOGE("tcgetattr failed");
-//        return;
-//    }
+    struct termios termios_p;
+    memset(&termios_p, 0, sizeof(struct termios));
+    if (-1 == tcgetattr(descriptor, &termios_p)) {
+        LOGE("fd: %d, tcgetattr failed", descriptor);
+        return;
+    }
 
     switch (stopBits) {
         case STOPBITS_1: {
             termios_p.c_cflag &= ~CSTOPB;
-            LOGD("stopBits: %d", stopBits);
+            LOGD("fd: %d, stopBits: %d", descriptor, stopBits);
             break;
         }
         case STOPBITS_2: {
             termios_p.c_cflag |= CSTOPB;
-            LOGD("stopBits: %d", stopBits);
+            LOGD("fd: %d, stopBits: %d", descriptor, stopBits);
             break;
         }
         default: {
-            LOGE("stopBits: invalid parameter");
+            LOGE("fd: %d, stopBits: invalid parameter", descriptor);
             return;
         }
     }
 
     if (-1 == tcsetattr(descriptor, TCSANOW, &termios_p)) {
-        LOGE("tcsetattr failed");
+        LOGE("fd: %d, tcsetattr failed", descriptor);
         return;
     }
 }
